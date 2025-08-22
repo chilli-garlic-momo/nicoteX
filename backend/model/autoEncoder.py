a4e0e74b1771dfb7d9cf3fd5443830f007bb5b82
chilli-garlic-momo <jaiswaljiya001@gmail.com>
2025-08-22 17:31:13 +0530

    data set generation

--- backend/model/autoEncoder.py
+++ backend/model/autoEncoder.py
@@ -66,5 +66,5 @@ def train_ae(df):
 
 if __name__ == "__main__":
     import numpy as np
-    df = pd.read_csv('backend/dataGen/synthetic_transactions.csv')
+    df = pd.read_csv('synthetic_transactions.csv')
     train_ae(df)
